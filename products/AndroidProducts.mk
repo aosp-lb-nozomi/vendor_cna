9bc1f5946e05a3a1c18ea8f91bdeb1ccd6aeaa60
djolivier <olivieer@users.noreply.github.com>
2014-02-17 23:49:02 +0100

    Update AndroidProducts.mk

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,19 +1,4 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cna_crespo.mk \
-    $(LOCAL_DIR)/cna_crespo4g.mk \
-    $(LOCAL_DIR)/cna_grouper.mk \
-    $(LOCAL_DIR)/cna_maguro.mk \
-    $(LOCAL_DIR)/cna_stingray.mk \
-    $(LOCAL_DIR)/cna_stingray_cdma.mk \
-    $(LOCAL_DIR)/cna_toro.mk \
-    $(LOCAL_DIR)/cna_toroplus.mk \
-    $(LOCAL_DIR)/cna_umts_everest.mk \
-    $(LOCAL_DIR)/cna_wingray.mk \
-    $(LOCAL_DIR)/cna_d2att.mk \
-    $(LOCAL_DIR)/cna_d2spr.mk \
-    $(LOCAL_DIR)/cna_d2tmo.mk \
     $(LOCAL_DIR)/cna_nozomi.mk \
-    $(LOCAL_DIR)/cna_aoba.mk \
-    $(LOCAL_DIR)/cna_d2usc.mk \
-    $(LOCAL_DIR)/cna_d2vzw.mk \
-    $(LOCAL_DIR)/cna_i9300.mk
+    $(LOCAL_DIR)/cna_aoba.mk
+
